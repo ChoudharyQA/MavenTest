6833cd0a08c3263ac52fe18ce61823a0662994cb
Saurabh <sourabh89556@gmail.com>
2024-02-02 18:29:32 +0530

    This is Maven test code

---
+++ src/test/java/MavenPractice/MavenTest/RestAssuredAPITest.java
@@ -0,0 +1,31 @@
+package MavenPractice.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredAPITest {
+
+	@Test
+	public void postJira() {
+		
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		
+		System.out.println("deleteTwitter");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/MavenPractice/MavenTest/SeleniumTest.java
@@ -0,0 +1,28 @@
+package MavenPractice.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void browserAutomaton() {
+		
+		System.out.println("browserAutomaton");
+		
+	}
+	
+	
+	@Test
+	public void elementUI() {
+		
+		System.out.println("elementUI");
+		
+	}
+	
+	
+	
+	
+	
+	
+}
